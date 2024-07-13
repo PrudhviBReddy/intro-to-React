105e7b93d381b9414136993aa55415bd5a1522b5
PrudhviBReddy <prudhvireddybokka1396@gmail.com>
2024-07-13 09:39:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React App</h1>
+      <h2>Indian Premier League</h2>
+      <p className="IPL">The Indian Premier League (IPL), also known as the TATA IPL for sponsorship reasons, is a men's Twenty20 (T20) cricket league held annually in India.<br></br>
+         Founded by the BCCI (the Board of Control for Cricket in India) in 2007, the league features ten state or city-based franchise teams.<br></br>
+          The IPL usually takes place during the summer, between March and May each year.<br></br>
+           It has an exclusive window in the ICC Future Tours Programme, resulting in fewer international cricket tours occurring during the IPL seasons.</p>
+    </div>
+  );
+}
+
+export default App;
